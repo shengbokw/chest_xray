d4da462b7200800259d49c599b065cad3920917b
verao-amy <amy402x@gmail.com>
2019-03-07 12:13:52 +0100

    add

--- scripts/lung_size.py
+++ scripts/lung_size.py
@@ -7,37 +7,38 @@ def read_the_image_file():
     matr = []
     f = open(X, "r").read().split("\n")[:-1]
     for number in f:
-        line = list(number)
+        line = list(map(int, list(number)))
         matr.append(line)
-    t = [[matr[j][i] for j in range(len(matr))] for i in range(len(matr[0]))]
-    return t
+    # t = [[matr[j][i] for j in range(len(matr))] for i in range(len(matr[0]))]
+    return matr
 
 
 def size_of_lungs():
     img = read_the_image_file()
+    t = [[img[j][i] for j in range(len(img))] for i in range(len(img[0]))]
     r = 0  # right (in actual body) lung
     l = 0
     s = 0  # starting point
     m = 0  # middle_separation point
-    for line in range(len(img)):
-        if img[line][:].count("1") > 0:
+    for line in range(len(t)):
+        if sum(t[line][:]) > 0:
             s = line
             break
         else:
             continue
-    for line in range(s, len(img)):
-        if img[line][:].count("1") == 0:
+    for line in range(s, len(t)):
+        if sum(t[line][:]) == 0:
             m = line
             break
         else:
-            s_r = img[line][:].count("1")
+            s_r = sum(t[line][:])
             r = r + s_r
-    for line in range(m, len(img)):
-        s_l = img[line][:].count("1")
+    for line in range(m, len(t)):
+        s_l = sum(t[line][:])
         l = l + s_l
 
     fraction = round(l/r, 3)
-    print("Right lung: %s" % r, "\nLeft lung size: %s" % l, "\nFraction between right and left lung: %s" % fraction)
+    # print("Right lung: %s" % r, "\nLeft lung size: %s" % l, "\nFraction between right and left lung: %s" % fraction)
     return r, l, fraction
 
 
---
+++ scripts/try.py
@@ -0,0 +1,166 @@
+import os
+import numpy as np
+import pandas as pd
+from keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator
+from skimage import morphology, io, exposure, img_as_float, transform, img_as_ubyte
+from matplotlib import pyplot as plt
+
+
+current_path = '/Users/shengbo/shengbo/VU/ML/chest_xray/lung-segmentation-2d/Demo/'
+folder = '/Users/dinghuiwen/PycharmProjects/Images/val/PNEUMONIA'
+files = [f for f in os.listdir(folder) if os.path.isfile(os.path.join(folder, f))]
+df = pd.DataFrame(data=files, columns={'img'})
+df[df['img'] == '.DS_Store'] = None
+df = df.dropna()
+# csv_path = current_path + 'idx.csv'
+# # Path to the folder with images. Images will be read from path + path_from_csv
+# path = current_path + 'Data/'
+
+def loadDataGeneral(df, path, im_shape):
+    """
+    reshaping images
+    """
+    X = []
+    for i, item in df.iterrows():
+        img = img_as_float(io.imread(path + '/' + item[0]))
+        img = transform.resize(img, im_shape)
+        img = exposure.equalize_hist(img)
+        img = np.expand_dims(img, -1)
+        X.append(img)
+    X = np.array(X)
+    X -= X.mean()
+    X /= X.std()
+
+    return X
+
+
+def remove_small_regions(img, size):
+    """
+    Morphologically removes small (less than size)
+    connected regions of 0s or 1s.
+    """
+    img = morphology.remove_small_objects(img, size)
+    img = morphology.remove_small_holes(img, size)
+
+    return img
+
+
+def distribution(gray_img):
+    """
+    calculate the distribution of img,
+    total will be used on calculating fraction
+    """
+    img_shape = gray_img.shape
+    gray_distribution = [0] * 256
+    total = 0
+    for i in range(0, img_shape[0]):
+        for j in range(0, img_shape[1]):
+            gray_distribution[int(gray_img[i][j])] += 1
+            total += int(gray_img[i][j])
+
+    return gray_distribution, total
+
+
+def lung_density(pr, img):
+    """calculate the density of two lungs."""
+    density = 0
+    size = 0
+    img_shape = img.shape
+    for i in range(0, img_shape[0]):
+        for j in range(0, img_shape[1]):
+            if pr[i][j] == 1:
+                size += 1
+                density += img[i][j]
+
+    return density * 1.0 / (size + 1)
+
+
+
+"""
+this function will combine features:
+gray scale value distribution,
+density of lungs,
+...
+"""
+features = []
+
+# Load test data
+im_shape = (256, 256)
+X = loadDataGeneral(df, folder, im_shape)
+
+inp_shape = X[0].shape
+
+# Load model
+model_name = 'trained_model.hdf5'
+UNet = load_model(model_name)
+
+# For inference standard keras ImageGenerator can be used.
+test_gen = ImageDataGenerator(rescale=1.)
+
+for xx in test_gen.flow(X, batch_size=1):
+    feature = []
+    img = exposure.rescale_intensity(np.squeeze(xx), out_range=(0, 1))
+    # I'm still thinking about how to deal with the gray scale
+    img = img_as_ubyte(img)
+    pred = UNet.predict(xx)[..., 0].reshape(inp_shape[:2])
+    pr = pred > 0.5
+    pr = remove_small_regions(pr, 0.02 * np.prod(im_shape))
+    pr_int = np.array(pr, dtype=np.int8)
+
+    dist, total = distribution(img)
+    feature.append(dist)
+    feature.append(total)
+    feature.append(lung_density(pr_int, img))
+
+    features.append(feature)
+    # np.savetxt('test.out', pr_int, delimiter='', fmt="%s")
+
+
+
+
+# plt.imshow(img, cmap='gray')
+# df = pd.read_csv(csv_path)
+
+
+"""visulization on test files"""
+# plt.figure(figsize=(10, 10))
+# for xx in test_gen.flow(X, batch_size=1):
+#     img = exposure.rescale_intensity(np.squeeze(xx), out_range=(0, 1))
+#     pred = UNet.predict(xx)[..., 0].reshape(inp_shape[:2])
+#
+#     pr = pred > 0.5
+#     ft = pred > 0.5
+#     ft.fill(False)
+#     pr = remove_small_regions(pr, 0.02 * np.prod(im_shape))
+#
+#     prs.append(pr)
+#
+#     print(df.iloc[i][0])
+#
+#     if i < 4:
+#         plt.subplot(4, 4, 4*i+1)
+#         plt.title('Processed ' + df.iloc[i][0])
+#         plt.axis('off')
+#         plt.imshow(img, cmap='gray')
+#
+#         plt.subplot(4, 4, 4*i+3)
+#         plt.title('Prediction')
+#         plt.axis('off')
+#         plt.imshow(pred, cmap='jet')
+#
+#         plt.subplot(4, 4, 4*i+4)
+#         plt.title('Difference')
+#         plt.axis('off')
+#         plt.imshow(np.dstack((pr.astype(np.int8), ft.astype(np.int8), pr.astype(np.int8))))
+#
+#     i += 1
+#     if i == n_test:
+#         break
+#
+# plt.tight_layout()
+# plt.savefig('results.png')
+# plt.show()
+#
+# pr_int = np.array(pr, dtype=np.int8)
+# np.savetxt('test.out', pr_int, delimiter='', fmt="%s")
